d7721321c6aac35755dda0ad9f964a4e056ed763
Theodore Messinezis <theo@theomessin.com>
2015-11-06 11:03:30 +0200

    Coded get runways from sector file

--- preferred-runway/PreferredRunwayPlugIn.cpp
+++ preferred-runway/PreferredRunwayPlugIn.cpp
@@ -2,7 +2,7 @@
 #include "PreferredRunwayPlugIn.h"
 
 #define MY_PLUGIN_NAME      "Preferred Runway Plugin"
-#define MY_PLUGIN_VERSION   "0.1"
+#define MY_PLUGIN_VERSION   "0.1.2"
 #define MY_PLUGIN_DEVELOPER "Theodore Messinezis"
 #define MY_PLUGIN_COPYRIGHT "MIT License (c) 2015"
 
@@ -10,18 +10,24 @@ PreferredRunwayPlugIn::PreferredRunwayPlugIn(void) : CPlugIn(EuroScopePlugIn::CO
 	MY_PLUGIN_NAME,
 	MY_PLUGIN_VERSION,
 	MY_PLUGIN_DEVELOPER,
-	MY_PLUGIN_COPYRIGHT)
-{
+	MY_PLUGIN_COPYRIGHT){
 
+	//Iterate through all runways from sector file and add them to the _RunwayData map
+	EuroScopePlugIn::CSectorElement RunwayIterator;
+	for (RunwayIterator = SectorFileElementSelectFirst(EuroScopePlugIn::SECTOR_ELEMENT_RUNWAY); RunwayIterator.IsValid(); RunwayIterator = SectorFileElementSelectNext(RunwayIterator, EuroScopePlugIn::SECTOR_ELEMENT_RUNWAY)) {
+		string AirportICAOCode = RunwayIterator.GetAirportName();
+		AirportICAOCode = AirportICAOCode.substr(0, 4);
+
+		runway Runway;
+		Runway.Heading = RunwayIterator.GetRunwayHeading(0);
+		Runway.Ident = RunwayIterator.GetRunwayName(0);
+		
+		_RunwayData[AirportICAOCode].push_back(Runway);
+	}
 }
 
 
 PreferredRunwayPlugIn::~PreferredRunwayPlugIn(){
-
-}
-
-void PreferredRunwayPlugIn::OnAsrContentLoaded(bool Loaded) {
-
 }
 
 bool PreferredRunwayPlugIn::OnCompileCommand(const char * sCommandLine) {
@@ -31,12 +37,33 @@ bool PreferredRunwayPlugIn::OnCompileCommand(const char * sCommandLine) {
 		return false;
 
 	string strICAO(sCommandLine + 5, sCommandLine + 9); ///< Strip ICAO code from sCommandLine
+	
+	string header = "Preferred Runway for " + strICAO;
 
-	DisplayUserMessage("Message", "Preferred Runway", getActiveRunway(strICAO).c_str(), true, true, false, true, false);
+	if (_RunwayData[strICAO].empty()) {
+		DisplayUserMessage("Preferred Runway", header.c_str(), "Undefined ICAO code.", true, true, false, true, false);
+		return true;
+	}
+
+	if (_WindData.find(strICAO) == _WindData.end()) {
+		string message = "No METAR data found for " + strICAO + " ICAO code. Please load METAR and try again...";
+		DisplayUserMessage("Preferred Runway", header.c_str(), message.c_str(), true, true, false, true, false);
+		return true;
+	}
+
+	DisplayUserMessage("Preferred Runway", header.c_str(), GetActiveRunway(strICAO).c_str(), true, true, false, true, false);
 
 	return true;
 }
 
-string PreferredRunwayPlugIn::getActiveRunway(string ICAO) {
-	return ICAO;
+string PreferredRunwayPlugIn::GetActiveRunway(string ICAO){
+	string CompiledWind = to_string(_WindData[ICAO].direction) + " degrees, " + to_string(_WindData[ICAO].speed) + " knots.";
+	return CompiledWind;
+}
+
+void PreferredRunwayPlugIn::OnNewMetarReceived(const char * sStation, const char * sFullMetar) {
+	string ICAO = sStation;
+	wind decodedWindData;
+
+	_WindData[ICAO] = decodedWindData;
 }
--- preferred-runway/PreferredRunwayPlugIn.h
+++ preferred-runway/PreferredRunwayPlugIn.h
@@ -1,9 +1,25 @@
 #pragma once
+#include <vector>
 #include <string>
+#include <map>
 #include <EuroScopePlugIn.h>
 #include "preferred-runway.h"
 
 using namespace std;
+
+struct wind {
+	int direction, speed;
+	wind(){
+		direction = 0;
+		speed = 0;
+	};
+};
+
+struct runway {
+	string Ident;
+	int Heading;
+};
+
 
 class PreferredRunwayPlugIn :
 	public EuroScopePlugIn::CPlugIn
@@ -12,13 +28,17 @@ public:
 	PreferredRunwayPlugIn();
 	virtual ~PreferredRunwayPlugIn();
 
-	inline virtual void OnAsrContentLoaded(bool Loaded);
-
 	inline virtual bool OnCompileCommand(const char * sCommandLine);
+
+	inline  virtual void OnNewMetarReceived(const char * sStation, const char * sFullMetar);
 
 	inline virtual void OnAsrContentToBeClosed(void) {
 		delete this;
 	};
 
-	inline virtual string getActiveRunway(string ICAO);
+	inline virtual string GetActiveRunway(string ICAO);
+
+private:
+	map<string, wind> _WindData;
+	map<string, vector<runway> > _RunwayData;
 };
